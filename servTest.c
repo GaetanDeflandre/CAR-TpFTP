bbe800af58d3a897fb3ad9ca76fea5584dbaac85
Grandsir Samuel <grandsir@a11p10.fil.univ-lille1.fr>
2015-02-05 11:32:01 +0100

    ajout de gestion d'erreur bind + socket pour le test

--- servTest.c
+++ servTest.c
@@ -4,6 +4,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <errno.h>
+
+#define BUF_SIZE 256
 
 
 int main(int argc, char ** argv)
@@ -11,9 +14,15 @@ int main(int argc, char ** argv)
 	int listenfd, clientfd;
 	struct sockaddr_in serv_addr;
 	
-	char buf[256];
+	char buf[BUF_SIZE];
 	
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if (listenfd == -1)
+	{
+		perror("erreur socket: ");
+		return 1;
+	}
 	
 	memset(&serv_addr, 0, sizeof(struct sockaddr));
 	memset(buf, 0, 256);
@@ -22,13 +31,23 @@ int main(int argc, char ** argv)
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_addr.sin_port = htons(2121);
 	
-	bind(listenfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+	if ((bind(listenfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))) != 0)
+	{
+		perror("erreur bind: ");
+		return 2;
+	}
 	
 	listen(listenfd, 10);
 	
 	clientfd = accept(listenfd, NULL, NULL);
-	snprintf(buf, sizeof(buf), "coucou\n");
+	snprintf(buf, sizeof(buf), "\n\n");
 	write(clientfd, buf, strlen(buf));
+	read(clientfd, buf, BUF_SIZE);
+	printf("%s\n", buf);
+	snprintf(buf, sizeof(buf), "\n");
+	write(clientfd, buf, strlen(buf));
+	//read(clientfd, buf, BUF_SIZE);
+	//~ printf("%s\n", buf);
 	close(clientfd);
 	
 	close(listenfd);
