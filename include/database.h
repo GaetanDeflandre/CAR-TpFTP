2020fab73230a7c762089720c0878bdbb251bfc9
Gaëtan DEFLANDRE <gaetan.deflandre@gmail.com>
2015-02-16 19:49:46 +0100

    ajout des fichiers de datanase

---
+++ include/database.h
@@ -0,0 +1,55 @@
+#ifndef DATABASE_H_
+#define DATABASE_H_
+
+/**
+ * @file database.h
+ * @brief Charge la base utilisateur en mémoire et gère 
+ * la base.
+ */
+
+/**
+ * @struct user_s 
+ * @brief La paire login et mot de passe de l'utilisateur. 
+ */
+struct user_s{
+    char* login;
+    char* password;
+};
+
+/**
+ * @typedef list_user_t
+ * @brief Liste des utilisateurs (login et mot de passe). 
+ */
+typedef struct user_s* list_user_t;
+
+
+/**
+ * Initialise la base de données.
+ *
+ * @return -1 en case d'erreur, sion 0.
+ */
+int init_database();
+
+/**
+ * Charge la base utilisateur en mémoire.
+ * 
+ * @param[out] database La base de données, une liste de 
+ *                      couple login et mot de passe.
+ * @return -1 en case d'erreur, sion 0.
+ */
+int load_database(list_user_t* database);
+
+/**
+ * Retourne vrai si l'utilisateur <code>login</code> pour 
+ * mot de passe <code>password</code> est dans la base et 
+ * que son mot de passe est correct.
+ *
+ * @param[in] login Le nom de l'utilisateur.
+ * @param[in] password Le mot de passe de l'utilisateur.
+ * @return 1 si l'utilisateur <code>login</code> pour 
+ * mot de passe <code>password</code> est dans la base et 
+ * que son mot de passe est correct, sinon retourne 0.
+ */
+unsigned accept(const char* login, const char* password);
+
+#endif /* DATABASE_H_ */
---
+++ src/database.c
@@ -0,0 +1,15 @@
+#include "database.h"
+
+#include <libxml2/libxml/tree.h>
+
+
+static list_user_t database;
+
+int init_database(){
+    
+}
+
+int load_database(list_user_t* database){
+    
+}
+
